481dbe836b48d0695de010d6b4406a156f2ad4c0
amarten <alexdmarten@gmail.com>
2022-01-22 18:25:27 -0800

    setup repo and installed npm packages. Added routes for landing page and notes page as well

---
+++ server.js
@@ -0,0 +1,44 @@
+/* HTML Routes Needed
+// Need a GET /notes that returns the notes.html
+
+// GET * should return the index.html file
+
+ */
+
+
+/* API Routes needed 
+// GET /api/notes should read the db.json file and return all saved notes as JSON
+
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
+    // Need to find a way to give each note a unique id when it is saved (npm packages should help with this)
+*/
+
+// import modules
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// add in middleware
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static('public'))
+
+// GET route for index.html
+app.get('/api/index', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+// GET route for notes.html
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+})
+
+
+
+
+
+// add listener for server events
+app.listen(PORT, () => {
+    console.log(`API server is active now on PORT ${PORT}`)
+})
