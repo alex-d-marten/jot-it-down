6c565b6349363188822cc33dd000374e3bea27ef
amarten <alexdmarten@gmail.com>
2022-01-23 13:18:42 -0800

    save new note and display on left hand side. Added id's to each note as well

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -43,6 +43,9 @@ const saveNote = (note) => {
     },
     body: JSON.stringify(note),
   })
+  .then(() => {
+    location.reload()
+  });
 }
 
 const deleteNote = (id) =>
@@ -74,10 +77,7 @@ const handleNoteSave = () => {
     title: noteTitle.value,
     text: noteText.value,
   };
-  saveNote(newNote).then(() => {
-    getAndRenderNotes();
-    renderActiveNote();
-  });
+  saveNote(newNote)
 };
 
 // Delete the clicked note
@@ -111,6 +111,7 @@ const handleNewNoteView = (e) => {
   renderActiveNote();
 };
 
+// show save button when text entered in note title or text area
 const handleRenderSaveBtn = () => {
   if (!noteTitle.value.trim() || !noteText.value.trim()) {
     hide(saveNoteBtn);
@@ -119,68 +120,9 @@ const handleRenderSaveBtn = () => {
   }
 };
 
-// Render the list of note titles
-const renderNoteList = async (notes) => {
-  let jsonNotes = await notes.json();
-  if (window.location.pathname === '/notes') {
-    noteList.forEach((el) => (el.innerHTML = ''));
-  }
-
-  let noteListItems = [];
-
-  // Returns HTML element with or without a delete button
-  const createLi = (text, delBtn = true) => {
-    const liEl = document.createElement('li');
-    liEl.classList.add('list-group-item');
-
-    const spanEl = document.createElement('span');
-    spanEl.classList.add('list-item-title');
-    spanEl.innerText = text;
-    spanEl.addEventListener('click', handleNoteView);
-
-    liEl.append(spanEl);
-
-    if (delBtn) {
-      const delBtnEl = document.createElement('i');
-      delBtnEl.classList.add(
-        'fas',
-        'fa-trash-alt',
-        'float-right',
-        'text-danger',
-        'delete-note'
-      );
-      delBtnEl.addEventListener('click', handleNoteDelete);
-
-      liEl.append(delBtnEl);
-    }
-
-    return liEl;
-  };
-
-  if (jsonNotes.length === 0) {
-    noteListItems.push(createLi('No saved Notes', false));
-  }
-
-  jsonNotes.forEach((note) => {
-    const li = createLi(note.title);
-    li.dataset.note = JSON.stringify(note);
-
-    noteListItems.push(li);
-  });
-
-  if (window.location.pathname === '/notes') {
-    noteListItems.forEach((note) => noteList[0].append(note));
-  }
-};
-
-// Gets notes from the db and renders them to the sidebar
-const getAndRenderNotes = () => getNotes().then(renderNoteList);
-
 if (window.location.pathname === '/api/notes') {
   saveNoteBtn.addEventListener('click', handleNoteSave);
   newNoteBtn.addEventListener('click', handleNewNoteView);
   noteTitle.addEventListener('keyup', handleRenderSaveBtn);
   noteText.addEventListener('keyup', handleRenderSaveBtn);
 }
-
-getAndRenderNotes();
--- server.js
+++ server.js
@@ -19,13 +19,14 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const parser = require('./lib/parser')
+const notes = require('./db/db.json')
+const fs = require('fs');
+
 
 // add in middleware
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static('public'))
-
-parser()
 
 // GET route for index.html
 app.get('/api/index', (req, res) => {
@@ -45,9 +46,28 @@ app.get('/public/assets/js/index.js', (req, res) => {
     res.sendFile(path.join(__dirname, './public/assets/js/index.js'))
 })
 
+function createNewNote(body, notesArray) {
+    const note = body;
+    console.log(note)
+    console.log(notesArray)
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(notesArray, null, 2)
+    )
+
+    // return finished code to post route for response
+    return note;
+}
+
 app.post('/notes', (req, res) => {
-    console.log(req.body)
+    req.body.id = notes.length.toString()
+    const note = createNewNote(req.body, notes)
+    res.json(note)
+    parser()
 })
+
+parser()
 
 // add listener for server events
 app.listen(PORT, () => {
